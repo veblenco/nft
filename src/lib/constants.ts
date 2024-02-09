00ea0c85a06a4cc6d3751b6c80ec41e61e93d2ae
Joe Blau <im@joeblau.com>
2024-02-09 15:46:26 -0800

    feat: add token constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,71 @@
+import { mainnet, polygon, polygonMumbai, base } from 'viem/chains';
+
+export const SUPERRARE_ADDRESS = {
+  [mainnet.id]: '0xb932a70A57673d89f4acfFBE830E8ed7f75Fb9e0',
+};
+
+export const HASHMASKS_CONTRACT = {
+  [mainnet.id]: '0xC2C747E0F7004F9E8817Db2ca4997657a7746928',
+};
+
+export const DECENTRALAND_ADDRESS = {
+  [mainnet.id]: '0xF87E31492Faf9A91B02Ee0dEAAd50d51d56D5d4d',
+};
+
+export const ZORA_ADDRESS = {
+  [mainnet.id]: '0xabEFBc9fD2F806065b4f3C237d4b59D9A97Bcac7',
+  [polygon.id]: '0x6953190AAfD8f8995e8f47e8F014d0dB83E92300',
+  [polygonMumbai.id]: '0xabEFBc9fD2F806065b4f3C237d4b59D9A97Bcac7',
+};
+
+export const FOUNDATION_ADDRESS = {
+  [mainnet.id]: '0x3B3ee1931Dc30C1957379FAc9aba94D1C48a5405',
+};
+
+export const MAKERSPLACE_ADDRESS = {
+  [mainnet.id]: '0x2A46f2fFD99e19a89476E2f62270e0a35bBf0756',
+};
+
+export const NOUNS_ADDRESS = {
+  [mainnet.id]: '0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03',
+};
+
+export const LIL_NOUNS_ADDRESS = {
+  [mainnet.id]: '0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B',
+};
+
+export const RARIBLE_ADDRESS = {
+  [mainnet.id]: '0x60F80121C31A0d46B5279700f9DF786054aa5eE5',
+};
+
+export const ENS_ADDRESS = {
+  [mainnet.id]: '0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85',
+};
+
+export const WRAPPED_CRYPTOPUNKS_ADDRESS = {
+  [mainnet.id]: '0xb7F7F6C52F2e2fdb1963Eab30438024864c313F6',
+};
+
+export const PUNKS_DATA_CONTRACT = {
+  [mainnet.id]: '0x16F5A35647D6F03D5D3da7b35409D65ba03aF3B2',
+};
+
+export const BLITMAP_ADDRESS = {
+  [mainnet.id]: '0x8d04a8c79cEB0889Bdd12acdF3Fa9D207eD3Ff63',
+};
+
+export const HEAVEN_COMPUTER_ADDRESS = {
+  [mainnet.id]: '0x80ADB36595239fe918c7D118C1F81e07d070801a',
+};
+
+export const POTION_ART_ADDRESS = {
+  [mainnet.id]: '0xdA98f59e1EDeCb2545d7b07B794e704ed6cF1f7A',
+};
+
+export const LOOT_ADDRESS = {
+  [mainnet.id]: '0xFF9C1b15B16263C61d017ee9F65C50e4AE0113D7',
+};
+
+export const AUTOGLYPHS_ADDRESS = {
+  [mainnet.id]: '0xd4e4078ca3495DE5B1d4dB434BEbc5a986197782',
+};
